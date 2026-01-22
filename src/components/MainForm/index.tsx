d4b215aee00f754f391627ca331205191942102b
ellen melo <ellen.melo156@gmail.com>
2026-01-22 17:23:17 -0300

    Enviando o estado via props por vários níveis de componentes (prop drilling)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,30 +1,50 @@
-import { PlayCircleIcon } from "lucide-react";
-import { Cycles } from "../Cycles";
-import { DefaultButton } from "../DefaultButton";
-import { DefaultInput } from "../DefaultInput";
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+import type { HomeProps } from '../../pages/Home';
 
-export function MainForm() {
-    return (
-        <form className="form" action=''>
-            <div className="formRow">
-                <DefaultInput
-                    labelText='task'
-                    id='meuInput'
-                    type='text'
-                    placeholder="Digite aqui"
-                />
-            </div>
-            <div className="formRow">
-                <p>Vou trabalhar em</p>
-            </div>
-            <div className="formRow">
-                <Cycles />
-            </div>
-            <div className="formRow">
-                <DefaultButton icon={<PlayCircleIcon />} />
-            </div>
-            
-        </form>
-    );
+export function MainForm({ state, setState }: HomeProps) {
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          workTime: 34,
+        },
+        formattedSecondsRemaining: '23:34',
+      };
+    });
+  }
+
+  return (
+    <form className='form' action=''>
+      <div>
+        <button type='button' onClick={handleClick}>
+          Clicar
+        </button>
+      </div>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Próximo intervalo é de {state.config.workTime}min</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
 }
-
