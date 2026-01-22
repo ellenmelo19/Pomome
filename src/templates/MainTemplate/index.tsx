e0225d8986e718f5dce0be401589b8522090fae2
ellen melo <ellen.melo156@gmail.com>
2026-01-22 14:56:23 -0300

    Definindo os models (modelos) da aplicação TaskModel e TaskStateModel

--- src/templates/MainTemplate/index.tsx
+++ src/templates/MainTemplate/index.tsx
@@ -1,9 +1,6 @@
-import "./styles/theme.css";
-import "./styles/global.css";
 import { Container } from "../../components/Container";
 import { Logo } from "../../components/Logo";
 import { Menu } from "../../components/Menu";
-
 import { Footer } from "../../components/Footer";
 
 type MainTemplateProps = {
