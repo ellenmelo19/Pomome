91fb3a7e391166b0a72e3f8177955660c2b39297
ellen melo <ellen.melo156@gmail.com>
2026-01-22 17:34:51 -0300

    O conceito de prop drilling (talvez você precise de Context API)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,50 +1,31 @@
-import { PlayCircleIcon } from 'lucide-react';
-import { Cycles } from '../Cycles';
-import { DefaultButton } from '../DefaultButton';
-import { DefaultInput } from '../DefaultInput';
-import type { HomeProps } from '../../pages/Home';
+import { PlayCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
+import { DefaultButton } from "../DefaultButton";
+import { DefaultInput } from "../DefaultInput";
 
-export function MainForm({ state, setState }: HomeProps) {
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: {
-          ...prevState.config,
-          workTime: 34,
-        },
-        formattedSecondsRemaining: '23:34',
-      };
-    });
-  }
+export function MainForm() {
 
-  return (
-    <form className='form' action=''>
-      <div>
-        <button type='button' onClick={handleClick}>
-          Clicar
-        </button>
-      </div>
-      <div className='formRow'>
-        <DefaultInput
-          labelText='task'
-          id='meuInput'
-          type='text'
-          placeholder='Digite algo'
-        />
-      </div>
-
-      <div className='formRow'>
-        <p>Próximo intervalo é de {state.config.workTime}min</p>
-      </div>
-
-      <div className='formRow'>
-        <Cycles />
-      </div>
-
-      <div className='formRow'>
-        <DefaultButton icon={<PlayCircleIcon />} />
-      </div>
-    </form>
-  );
+    return (
+        <form className="form" action=''>
+            <div className="formRow">
+                <DefaultInput
+                    labelText='task'
+                    id='meuInput'
+                    type='text'
+                    placeholder="Digite aqui"
+                />
+            </div>
+            <div className="formRow">
+                <p>Vou trabalhar em 25 min </p>
+            </div>
+            <div className="formRow">
+                <Cycles />
+            </div>
+            <div className="formRow">
+                <DefaultButton icon={<PlayCircleIcon />} />
+            </div>
+            
+        </form>
+    );
 }
+
