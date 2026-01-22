aa86b853bf54757038d555c3873c5fbd27dab3e7
ellen melo <ellen.melo156@gmail.com>
2026-01-22 16:13:01 -0300

    Enviando o estado via props por vários níveis de componentes (prop drilling)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/Countdown";
 import { MainForm } from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
+type HomeProps = {
+    state: TaskStateModel,
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>,
+};
 
-export function Home() {
+export function Home(props: HomeProps) {
+    const { state, setState } =  props;
+
+
     return (
         <MainTemplate>
             <Container>
@@ -14,7 +22,6 @@ export function Home() {
             <Container>
                 <MainForm />
             </Container>
-            
         </MainTemplate>
     );
 }
