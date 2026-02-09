5cf79844228da8b108dbd19bdbd169485aeb2adc
ellen melo <ellen.melo156@gmail.com>
2026-02-09 13:02:58 -0300

    Criei meu router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Home } from "./pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { MessagesContainer } from "./MessagesContainer";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { NotFound } from "./pages/Not Found";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
+
+import { MainRouter } from "./routers/MainRouter";
 
 export function App() {
 
 	return (
 		<TaskContextProvider>
 			<MessagesContainer>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-
-
-						<Route path="*" element={<NotFound />} />							
-					</Routes>
-				</BrowserRouter>
+				<MainRouter />
 			</MessagesContainer>
 		</TaskContextProvider>
 	);
