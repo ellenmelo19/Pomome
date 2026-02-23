f35817667ad4de913016f78132625fad440c75e6
ellen melo <ellen.melo156@gmail.com>
2026-02-23 09:15:10 -0300

    correcao de bug no tips

--- src/components/Tips/index.tsx
+++ src/components/Tips/index.tsx
@@ -2,23 +2,26 @@ import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
 
-
 export function Tips() {
     const {state} = useTaskContext();
-      const nextCycle = getNextCycle(state.currentCycle);
-      const nextCycleType = getNextCycleType(nextCycle);
-        
-    const tipsForWhenActiveTask = {
-           workTime: <span> Foque por <b>{state.config.workTime} minutos</b></span>,
-           shortBreaktime: ( <span> Descanse por <b>{state.config.shortBreakTime}</b> minutos</span>),
-           longBreakTime: <span> Descanso <b>longo</b></span>,
-       }
+    const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle);
     
-       const tipsForNoActiveTask = {
-           workTime: <span> Próximo ciclo é de <b>{state.config.workTime}</b> minutos</span>,
-           shortBreaktime: ( <span> Próximo descanso é de <b>{state.config.shortBreakTime}</b> minutos</span>),
-           longBreakTime: <span> Próximo descanso será <b>longo</b></span>,
-       }
+    const tipsForWhenActiveTask = {
+        workTime: <span> Foque por <b>{state.config.workTime} minutos</b></span>,
+        shortBreakTime: ( // Corrigido: era shortBreaktime, agora é shortBreakTime
+            <span> Descanse por <b>{state.config.shortBreakTime}</b> minutos</span>
+        ),
+        longBreakTime: <span> Descanso <b>longo</b></span>,
+    }
+    
+    const tipsForNoActiveTask = {
+        workTime: <span> Próximo ciclo é de <b>{state.config.workTime}</b> minutos</span>,
+        shortBreakTime: ( // Corrigido: era shortBreaktime, agora é shortBreakTime
+            <span> Próximo descanso é de <b>{state.config.shortBreakTime}</b> minutos</span>
+        ),
+        longBreakTime: <span> Próximo descanso será <b>longo</b></span>,
+    }
 
     return (
         <> 
